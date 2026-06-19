b047ed0bdeeff9d1bc8f086e3f07c4aba9e6bb76
Fatih <fatihsakaryaa7@gmail.com>
2026-06-19 23:58:22 +0300

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "skrybucket1907"
     key            = "prod/terraform.tfstate"
     region         = "eu-north-1"
-    use_lockfile   = true  
+    dynamodb_table = "de-souza"  # <-- use_lockfile yerine bunu ekleyin
     encrypt        = true
   }
 }
