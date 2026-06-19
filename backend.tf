96e78623b8032bc08be3eb5072c98af2b0d90e17
Fatih <fatihsakaryaa7@gmail.com>
2026-06-19 23:58:36 +0300

    Update dynamodb_table configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "skrybucket1907"
     key            = "prod/terraform.tfstate"
     region         = "eu-north-1"
-    dynamodb_table = "de-souza"  # <-- use_lockfile yerine bunu ekleyin
+    dynamodb_table = "de-souza"  
     encrypt        = true
   }
 }
